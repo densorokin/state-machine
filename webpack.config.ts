2f685f7eeeea1247ee4708b120a51c8d16313915
Denis Sorokin <densorokin@outlook.com>
2024-02-14 16:21:38 +0200

    chore: fix webpack.config styles

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,27 @@
 import path from 'path';
-import webpack, {Configuration} from 'webpack';
+import webpack, { Configuration } from 'webpack';
 
 const config: Configuration = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.ts"),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true
   },
   plugins: [new webpack.ProgressPlugin()],
   resolve: {
-    extensions: [".ts"],
+    extensions: ['.ts']
   },
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
 
 export default config;
