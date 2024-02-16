9fdff97dcbe3e1646ba530ac857b8c2715ea6ce4
Denis Sorokin <densorokin@outlook.com>
2024-02-16 15:28:22 +0200

    refactor: updated patch version

--- src/hooks/useStateMachine.tsx
+++ src/hooks/useStateMachine.tsx
@@ -3,12 +3,15 @@ import { createMachine } from '../stateMachine';
 import { useEffect } from 'react';
 
 export const useStateMachine = (initState: string, transitions: any, options: any) => {
+  console.log('useStateMachine.tsx >>> start');
   const machine = createMachine(initState, transitions);
 
   const [machineState, setMachineState] = useState(machine.state);
 
   useEffect(() => {
+    console.log('useStateMachine.tsx >>> useEffect');
     if (options.commonSubscribe) {
+      console.log('useStateMachine.tsx >>> subscription');
       machine.subscribe((state) => setMachineState(state));
     }
   }, []);
